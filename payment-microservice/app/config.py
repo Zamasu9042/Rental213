a7fe0dfdc974c1dc6899fd85a4d92a35af43e902
gejunchim2024 <gejun.chim.2024@smu.edu.sg>
2026-03-25 16:48:34 +0800

    updated

--- payment-microservice/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY")
-    STRIPE_PUBLISHABLE_KEY = os.getenv("STRIPE_PUBLISHABLE_KEY")
-    DEBUG = os.getenv("FLASK_DEBUG", "false").lower() == "true"
