c9324f5b0fe4bdc38282103cca191242161719c8
shihanseah-ssh <shihan.seah.2024@smu.edu.sg>
2026-04-07 09:31:08 +0800

    hopefully no clash

---
+++ backend/payment-service/payment-db.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+load_dotenv()
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "mysql+pymysql://payment:payment_pass@localhost:3312/payment_db",
+)
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
