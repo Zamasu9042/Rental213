7aa3fd1290ca537ba68fff6f1de985c541a8c4ca
rnny123 <cshinron@gmail.com>
2026-04-08 07:22:14 +0800

    nice

---
+++ backend/account-service/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+
+from database import Base
+
+
+class Account(Base):
+    __tablename__ = "account"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    account_id = Column(Integer, nullable=False, unique=True, index=True)
+    account_name = Column(String(255), nullable=False)
+    phone_no = Column(String(64), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    password_hash = Column(String(255), nullable=False)
+    role = Column(String(32), nullable=False, default="renter")
