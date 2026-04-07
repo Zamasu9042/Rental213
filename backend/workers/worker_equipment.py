4b60224efb21c7d95b9a6ff91ed980f00000b0ad
shihanseah-ssh <shihan.seah.2024@smu.edu.sg>
2026-04-07 09:47:59 +0800

    small edits to ports

--- backend/workers/worker_equipment.py
+++ backend/workers/worker_equipment.py
@@ -9,19 +9,19 @@ import asyncio
 import httpx
 from pyzeebe import ZeebeWorker, create_camunda_cloud_channel
 
-CAMUNDA_CLIENT_ID     = "YOUR_CLIENT_ID"
-CAMUNDA_CLIENT_SECRET = "YOUR_CLIENT_SECRET"
-CAMUNDA_CLUSTER_ID    = "YOUR_CLUSTER_ID"
-CAMUNDA_REGION        = "ont-1"
+CAMUNDA_CLIENT_ID     = "AQazmsVXl7idqPlY1pMGm~Zh7Gv3Lgxk"
+CAMUNDA_CLIENT_SECRET = "uH0.fzO7HfUxQ0FTlEe0DsBRe-WVgbxIq_BD0H8rpw422I.1Ig.jGZ~9JVZiU0R8"
+CAMUNDA_CLUSTER_ID    = "db920878-5333-4352-b103-0803eb907686"
+CAMUNDA_REGION        = "sin-2"
 
-EQUIPMENT_SERVICE_URL = "http://localhost:5003"
+EQUIPMENT_SERVICE_URL = "http://localhost:8001"
 
 async def main():
     channel = create_camunda_cloud_channel(
         client_id=CAMUNDA_CLIENT_ID,
         client_secret=CAMUNDA_CLIENT_SECRET,
         cluster_id=CAMUNDA_CLUSTER_ID,
-        region=CAMUNDA_REGION" 
+        region=CAMUNDA_REGION
     )
     worker = ZeebeWorker(channel)
 
